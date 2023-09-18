5c9b8fd0e6df8e54b2908525b747584df509d6cf
felipe willy <felipe00cj@gmail.com>
2023-09-18 17:31:11 -0300

    Aplicando condições

--- desafio-sist-banco.py
+++ desafio-sist-banco.py
@@ -11,25 +11,59 @@ menu = """
 
     |==> """.lower()
 
-
 #Regras de negocio
-saldo = 0
+saldo = 100
 limite = 500
 numero_saques = 0
 LIMITE_SAQUES = 3
+extrato = []
 
 while True:
 
     opcao = input(menu)
 
     if opcao == 'd':
-        ...
-    
+        
+        deposito = float(input('Valor [D]eposito: '))
+
+        if  deposito > 0:
+            saldo += deposito
+
     elif opcao == 's':
-        ...
+
+        print('-='*15)
+        print(f'Saldo atual na conta: {saldo}')
+        print('-='*15)
+
+        if numero_saques < LIMITE_SAQUES:
+            
+            numero_saques += 1
+
+            saque = float(input('Valor de saque: '))
+
+            if saque > 500:
+                
+                print('Valor saque maior que limite permitido R$ 500,00 ')
+
+            else:
+
+                if saldo - saque < 0:
+
+                    print(f'saque indisponivel {saque}  saldo conta: {saldo}')
+
+                else:
+
+                    saldo = saldo - saque
+                    extrato.append(saque)
+                    
+        else:
+
+            print('Limite saque atingido no dia.')
+        
 
     elif opcao == 'e':
-        ...
+
+        print(extrato)
     
     elif opcao == 'q':
         break
