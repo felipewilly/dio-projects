10171446e9e8f66b1cae620c838b10c061f08c63
FelipeWilly <felipe00cj@gmail.com>
2023-09-21 23:03:51 -0300

    Refatorando codigo desafio Função

--- desafio-sist-banco.py
+++ desafio-sist-banco.py
@@ -2,71 +2,94 @@
 Sistema Bancário, desafio dio.me, nesse desafio irei seguir conforme aulas apresentado para caminhar por todo o percurso já aprendido em python
 '''
 
+def menu():
 
-menu = """
-    [d] Depositar
-    [s] Sacar
-    [e] Extrato
-    [q] Sair
+    esc = input("""
+        [d] Depositar
+        [s] Sacar
+        [e] Extrato
+        [q] Sair
 
-    |==> """.lower()
+        |==> """).lower()
 
-#Regras de negocio
-saldo = 100
-limite = 500
-numero_saques = 0
-LIMITE_SAQUES = 3
-extrato = []
+    return esc
 
-while True:
-
-    opcao = input(menu)
-
-    if opcao == 'd':
-        
+def deposito(user):
+    
         deposito = float(input('Valor [D]eposito: '))
-
+        
         if  deposito > 0:
-            saldo += deposito
+            user['saldo'] += deposito
 
-    elif opcao == 's':
+def sacar():
+    ...
 
-        print('-='*15)
-        print(f'Saldo atual na conta: R${saldo}')
-        print('-='*15)
+def extrato():
+    ...
 
-        if numero_saques < LIMITE_SAQUES:
-            
-            numero_saques += 1
 
-            saque = float(input('Valor de saque: '))
+def main(user):
 
-            if saque > 500:
-                
-                print('Valor saque maior que limite permitido R$ 500,00 ')
+    limite_saque = 3
 
-            else:
+    while True:
 
-                if saldo - saque < 0:
+        opcao = menu()
 
-                    print(f'saque indisponivel R${saque}  saldo conta: R${saldo}')
+        if opcao == 'd':
+
+            deposito(user=user)
+
+
+        elif opcao == 's':
+
+            print('-='*15)
+            print(f'Saldo atual na conta: R${ user["saldo"] }')
+            print('-='*15)
+
+            if user['numero_saques'] < limite_saque:
+
+                numero_saques += 1
+
+                saque = float(input('Valor de saque: '))
+
+                if saque > 500:
+
+                    print('Valor saque maior que limite permitido R$ 500,00 ')
 
                 else:
 
-                    saldo = saldo - saque
-                    extrato.append(saque)
-                    
+                    if saldo - saque < 0:
+
+                        print(f'saque indisponivel R${saque}  saldo conta: R${saldo}')
+
+                    else:
+
+                        saldo = saldo - saque
+                        extrato.append(saque)
+
+            else:
+
+                print('Limite saque atingido no dia.')
+
+
+        elif opcao == 'e':
+
+            print(extrato)
+
+        elif opcao == 'q':
+            break
+
         else:
+            print('Opção digitada incorreta tente outra OP ')
 
-            print('Limite saque atingido no dia.')
-        
 
-    elif opcao == 'e':
+if __name__ == "__main__":
 
-        print(extrato)
+    cliente = {
+        'saldo' : 100,
+        'limite' : 500,
+        'numero_saques': 0,
+        'extrato':[] } 
     
-    elif opcao == 'q':
-        break
-
-    else:
-        print('Opção digitada incorreta tente outra OP ')
+    main(user=cliente)
